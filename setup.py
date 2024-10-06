ee3fbbd7f36a2d779aadde5274c64d79a3ffd300
Arata Tanaka <ky.37f.9850@s.thers.ac.jp>
2024-10-06 10:47:18 +0900

    setup

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from distutils.core import setup
 
+def parse_requirements(filename):
+    with open(filename, 'r') as f:
+        return f.read().splitlines()
+
 setup(
     name='dsec_det',
     packages=['dsec_det'],
-    package_dir={'':'src'}        
+    package_dir={'': 'src'},
+    install_requires=parse_requirements('requirements.txt')
 )
